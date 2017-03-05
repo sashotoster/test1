fac64bec16a3de2736fe72ad774985f5bb056eda
sashotoster <sashotoster@gmail.com>
2017-03-05 19:30:00 +0300

    initial commit

---
+++ ya1.js
@@ -0,0 +1,77 @@
+$(function () {
+	
+	//var mcth = $('.main-content>.test-content').height();
+	
+	
+	//$('.sidebar').height($('.main-content').height());
+    var middleHeight = $('.middle').height(), 
+        sidebarContentBottom = $('.sidebar>.test-content').offset().top + $('.sidebar>.test-content').height();
+	
+	$(window).scroll(function () {
+		var scTop = $(window).scrollTop();
+		if (scTop > sidebarContentBottom) {
+			if (!$('.sidebar').hasClass('faded')) {
+				$('.sidebar').addClass('faded');
+				$('.sidebar').fadeOut();
+				setMinHeight('.main-content', middleHeight);
+			}		
+		} else {
+			if ($('.sidebar').hasClass('faded')) {
+				$('.sidebar').removeClass('faded');
+				$('.sidebar').fadeIn();				
+			}
+		}
+	});
+	
+	$('.login').click(function () {
+		$('.main').addClass('covered');
+		$('.popup').css('display', 'inline-block');
+		disableScroll();
+	});
+	$('.cancel').click(function () {
+		$('.main').removeClass('covered');
+		$('.popup').css('display', 'none');
+		enableScroll();
+	});	
+	
+	function setMinHeight (el, h) {
+		$(el).css('min-height', middleHeight);
+	}
+	
+
+	var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+	function preventDefault(e) {
+	  e = e || window.event;
+	  if (e.preventDefault)
+		  e.preventDefault();
+	  e.returnValue = false;  
+	}
+
+	function preventDefaultForScrollKeys(e) {
+		if (keys[e.keyCode]) {
+			preventDefault(e);
+			return false;
+		}
+	}
+
+	function disableScroll() {
+	  if (window.addEventListener) // older FF
+		  window.addEventListener('DOMMouseScroll', preventDefault, false);
+	  window.onwheel = preventDefault; // modern standard
+	  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+	  window.ontouchmove  = preventDefault; // mobile
+	  document.onkeydown  = preventDefaultForScrollKeys;
+	}
+
+	function enableScroll() {
+		if (window.removeEventListener)
+			window.removeEventListener('DOMMouseScroll', preventDefault, false);
+		window.onmousewheel = document.onmousewheel = null; 
+		window.onwheel = null; 
+		window.ontouchmove = null;  
+		document.onkeydown = null;  
+	}	
+	
+	
+});
