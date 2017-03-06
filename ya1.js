00cfab8ee6ad22778041433ca253cefa8aae66f6
sashotoster <sashotoster@gmail.com>
2017-03-06 23:52:41 +0300

    changes in scrolling disable

--- ya1.js
+++ ya1.js
@@ -23,52 +23,25 @@ $(function () {
 	$('.login').click(function () {
 		$('.main').addClass('covered');
 		$('.popup').css('display', 'inline-block');
-		disableScroll();
+        $('html, body').css({
+            overflow: 'hidden',
+            height: '100%'
+        });
+		//disableScroll();
 	});
 	$('.cancel').click(function () {
 		$('.main').removeClass('covered');
 		$('.popup').css('display', 'none');
-		enableScroll();
+             $('html, body').css({
+                overflow: 'auto',
+                height: 'auto'
+            });       
+		//enableScroll();
 	});	
 	
 	function setMinHeight (el, h) {
 		$(el).css('min-height', middleHeight);
 	}
-	
-
-	var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-	function preventDefault(e) {
-	  e = e || window.event;
-	  if (e.preventDefault)
-		  e.preventDefault();
-	  e.returnValue = false;  
-	}
-
-	function preventDefaultForScrollKeys(e) {
-		if (keys[e.keyCode]) {
-			preventDefault(e);
-			return false;
-		}
-	}
-
-	function disableScroll() {
-	  if (window.addEventListener) // older FF
-		  window.addEventListener('DOMMouseScroll', preventDefault, false);
-	  window.onwheel = preventDefault; // modern standard
-	  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-	  window.ontouchmove  = preventDefault; // mobile
-	  document.onkeydown  = preventDefaultForScrollKeys;
-	}
-
-	function enableScroll() {
-		if (window.removeEventListener)
-			window.removeEventListener('DOMMouseScroll', preventDefault, false);
-		window.onmousewheel = document.onmousewheel = null; 
-		window.onwheel = null; 
-		window.ontouchmove = null;  
-		document.onkeydown = null;  
-	}	
 	
 	
 });
